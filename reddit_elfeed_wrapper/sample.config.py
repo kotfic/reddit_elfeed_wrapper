46d4948d0913ccbcd740a722c6bb52ef2df245d5
Christopher Kotfila <kotfic@gmail.com>
2015-03-18 14:38:46 -0400

    Reddit wrapper

---
+++ reddit_elfeed_wrapper/sample.config.py
@@ -0,0 +1,6 @@
+USER_AGENT = "Praw user-agent"
+CLIENT_ID = "#######"
+CLIENT_SECRET = "#######################"
+AUTH_REDIRECT_URI = "http://127.0.0.1:65010/authorize_callback"
+
+CODE = "###################"
